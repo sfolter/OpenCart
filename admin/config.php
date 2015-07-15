ba969321320bdbdb9911cc4fbc2f22a585e049e7
Miroslav Kovachev <contact@mkspace.net>
2015-07-16 00:41:12 +0300

    First comit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/root/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/root/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/root/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/root/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/root/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/root/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/root/system/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/root/system/download/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/root/system/upload/');
+define('DIR_LOGS', 'C:/xampp/htdocs/root/system/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/root/system/modification/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/root/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'darkmaster');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
